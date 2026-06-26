f225a0d867ecba1bac987a2975742e1d0a59461b
Lavanya Middha <lavanyamiddha@gmail.com>
2026-06-26 17:13:33 -0400

    Adding knowledge base files

---
+++ backend/database/connectors/sqlite-connector.py
@@ -0,0 +1,10 @@
+# import sqlite3
+
+
+# class SQLiteConnector:
+#     def __init__(self):
+#         self.conn = sqlite3.connect("mydatabase.db")
+#         self.cursor = self.conn.cursor()
+
+#     def executeStatement(self, stmt:str):
+#         return 
